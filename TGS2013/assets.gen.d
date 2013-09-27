cdeb1efd258d3a6ada5b614721bf5cd0080b99d5
Jean Brunier <brunier42@gmail.com>
2013-09-27 11:44:44 +0200

    Revert "add gitignore"
    This reverts commit 8af579514341a0c780dbe2025f9534468f12b52c.

--- TGS2013/assets.gen.d
+++ TGS2013/assets.gen.d
@@ -1,55 +1,55 @@
 assets.gen.o: assets.gen.cpp \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/asset.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/asset/group.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/abi.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/syscall.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/types.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/audio.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/events.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/vram.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/asset.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/elf.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/limits.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/macros.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/math.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/asset/loader.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/array.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/memory.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/asset/audio.h \
-  C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/asset/image.h
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/asset.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/asset/group.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/abi.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/syscall.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/types.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/audio.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/events.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/vram.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/asset.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/elf.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/limits.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/macros.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/math.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/asset/loader.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/array.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/memory.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/asset/audio.h \
+  C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/asset/image.h
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/asset.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/asset.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/asset/group.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/asset/group.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/abi.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/abi.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/syscall.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/syscall.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/types.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/types.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/audio.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/audio.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/events.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/events.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/vram.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/vram.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/asset.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/asset.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/elf.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/abi/elf.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/limits.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/limits.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/macros.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/macros.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/math.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/math.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/asset/loader.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/asset/loader.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/array.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/array.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/memory.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/memory.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/asset/audio.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/asset/audio.h:
 
-C:\Users\DANEEL\Documents\GitHub\SaveDatKitten\/include\sifteo/asset/image.h:
+C:\Users\baron\Documents\GitHub\SaveDatKitten\/include\sifteo/asset/image.h:
