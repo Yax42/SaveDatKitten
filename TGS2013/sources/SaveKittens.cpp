6fec3b6b3f8842b959e8bba4c8d1b68bf30bb7dc
Paul Baron <paulbaron7993@gmail.com>
2013-10-24 15:19:18 +0200

    -Try to fix the glitch

--- TGS2013/sources/SaveKittens.cpp
+++ TGS2013/sources/SaveKittens.cpp
@@ -24,13 +24,20 @@ void SaveKittens::init()
 void SaveKittens::update(Sifteo::TimeDelta dt)
 {
 	_player.update(dt);
-	//_player.print(_player);
+	_player.print(_drawer, _player.x(), _player.y());
+	_map.printCase(_player.cube(), _drawer, _player.x(), _player.y());
+	_drawer.flush(_player.cube());
+	if (_player.x() != _player.xOld() || _player.y() != _player.yOld())
+	{
+		_player.print(_drawer, _player.xOld(), _player.yOld());
+		_map.printCase(_player.cubeOld(), _drawer, _player.xOld(), _player.yOld());
+		_drawer.flush(_player.cubeOld());
+	}
 }
 
 void SaveKittens::onNeighborAdd(unsigned firstID, unsigned firstSide, unsigned secondID, unsigned secondSide)
 {
 	_player.connection(&_cubes[firstID], firstSide, &_cubes[secondID], secondSide);
-	_map.printCase(_player.cube(), _drawer, _player.x(), _player.y());
-	_drawer.flush(_player.cube());
 	LOG("now in case %d %d\n", _player.x(), _player.y());
+	_drawer.flush(_player.cube());
 }
