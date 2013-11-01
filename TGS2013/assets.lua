e2216a560e580f47b73b69830e490668ec1bb45a
Jean Brunier <brunier42@gmail.com>
2013-11-01 14:47:52 +0100

    pikachu

--- TGS2013/assets.lua
+++ TGS2013/assets.lua
@@ -2,13 +2,13 @@ IconAssets = group{quality=10}
 Icon = image{"resources/icon.png"}
 
 GameAssets = group{}
-GroundTiles = image{"resources/tiles.png", pinned=true, width=128, height=128}
-Pikachu = image{"resources/pikachu.png", pinned=true, width=32, height=32}
-Plants = image{"resources/plants.png", pinned=true, width=64, height=64}
-VerticalWall = image{"resources/sideWall.png"}
-HorizontalWall = image{"resources/topWall.png"}
-BlackTile = image{"resources/black.png"}
-IsHere = image{"resources/isHere.png"}
+GroundTiles = image{"resources/tiles.png", pinned=true, width=128, height=128, quality=0}
+Pikachu = image{"resources/pikachu.png", pinned=true, width=32, height=32, quality=1}
+Plants = image{"resources/plants.png", pinned=true, width=64, height=64, quality=1}
+VerticalWall = image{"resources/sideWall.png", quality=1}
+HorizontalWall = image{"resources/topWall.png", quality=1}
+BlackTile = image{"resources/black.png", quality=1}
+IsHere = image{"resources/isHere.png", quality=1}
 
 CatAssets = group{}
 Cat = image{"resources/cat.png", pinned=true, width=32, height=32}
